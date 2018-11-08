380d43881c1643d5d5f5cf4c49cd173f3bfdfc98
cutajarj2 <james.cutajar@blip.pt>
2018-11-08 09:56:20 +0000

    Adding ex3 solution, streaming primes

--- src/main/scala/com/cutajarjames/exercise3/StreamingPrimes.scala
+++ src/main/scala/com/cutajarjames/exercise3/StreamingPrimes.scala
@@ -1,5 +1,8 @@
 package com.cutajarjames.exercise3
 
 class StreamingPrimes {
-  val allPrimes: Stream[Int] = 2 #:: Stream.from(3)
+  val allPrimes: Stream[Int] = 2 #:: Stream.from(3).filter { c =>
+    val primesUptoSqrt = allPrimes.takeWhile(p => p <= math.sqrt(c))
+    !primesUptoSqrt.exists(p => c % p == 0)
+  }
 }
