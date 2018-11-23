aeaea0f3bb685fd9f3fa6b5f372a580bf89af6aa
cutajarj2 <james.cutajar@blip.pt>
2018-11-23 17:32:23 +0000

    Adding secret santa problem as extra exercise.

---
+++ src/main/scala/com/cutajarjames/exercise4/SecretSanta.scala
@@ -0,0 +1,21 @@
+package com.cutajarjames.exercise4
+
+import scala.util.Random
+
+/**
+  * Implement the secret santa algorithm.
+  * The method accepts a list of names and outputs a random pairing, of who's buying
+  * a xmas present to whom.
+  * Everyone should buy and receive only one gift
+  *
+  * Examples:
+  * generateGiftPairing(List("James", "Ruth", "Isabel") should return a random pairing such as
+  *   Map("Ruth" -> "James", "Isabel" -> "Ruth", "James" -> "Isabel")
+  */
+class SecretSanta {
+  def generateGiftPairing(names: List[String]): Map[String, String] = {
+    val shuffledList = Random.shuffle(names)
+    shuffledList.zip(shuffledList.tail ::: shuffledList.headOption.toList)
+      .toMap
+  }
+}
