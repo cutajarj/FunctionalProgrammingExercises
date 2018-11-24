166a881ee7f617fee7551f476d16b996b3e6ae63
cutajarj2 <james.cutajar@blip.pt>
2018-11-24 17:17:55 +0000

    Adding hint to Steaming Primes

--- src/main/scala/com/cutajarjames/exercise3/StreamingPrimes.scala
+++ src/main/scala/com/cutajarjames/exercise3/StreamingPrimes.scala
@@ -9,6 +9,8 @@ package com.cutajarjames.exercise3
   *   The number 9 is not a prime numbers because 3 is a factor, and 3 is less or equal than sqrt(9)
   *   The number 11 is a prime number because the prime numbers 2, 3 (less or equal than sqrt(11)) are not factors
   *   The number 97 is a prime number because the prime numbers 2, 3, 5 and 7 (less or equal than sqrt(97)) are not factors
+  *
+  *   HINT: Think recursive streams!
   */
 class StreamingPrimes {
   val allPrimes: Stream[Int] = 2 #:: Stream.from(3)
