0261582584f355c8c9b902b99afc061d579e74d2
cutajarj2 <james.cutajar@blip.pt>
2018-11-25 16:18:46 +0000

    Adding spec for secret santa

--- src/main/scala/com/cutajarjames/exercise4/SecretSanta.scala
+++ src/main/scala/com/cutajarjames/exercise4/SecretSanta.scala
@@ -11,6 +11,8 @@ import scala.util.Random
   * Examples:
   * generateGiftPairing(List("James", "Ruth", "Isabel") should return a random pairing such as
   *   Map("Ruth" -> "James", "Isabel" -> "Ruth", "James" -> "Isabel")
+  *
+  *   Assume the input list will always contain at least one name
   */
 class SecretSanta {
   def generateGiftPairing(names: List[String]): Map[String, String] = {
--- src/test/scala/com/cutajarjames/exercise4/SecretSantaSpec.scala
+++ src/test/scala/com/cutajarjames/exercise4/SecretSantaSpec.scala
@@ -3,5 +3,26 @@ package com.cutajarjames.exercise4
 import org.scalatest.{FlatSpec, Matchers}
 
 class SecretSantaSpec extends FlatSpec with Matchers {
+  it should "output the correct pairing for 3 items" in {
+    val pairing = (new SecretSanta).generateGiftPairing(List("James", "Ruth", "Isabel"))
+    pairing.keys shouldEqual Set("James", "Ruth", "Isabel")
+    pairing.values.size shouldEqual 3
+    pairing.values.toSet shouldEqual Set("James", "Ruth", "Isabel")
+    pairing.foreach{case (v,k) => v shouldNot be(k)}
+  }
+
+  it should "output the correct pairing for 10 items" in {
+    val pairing = (new SecretSanta).generateGiftPairing(List("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"))
+    pairing.keys shouldEqual Set("a", "b", "c", "d", "e", "f", "g", "h", "i", "k")
+    pairing.values.size shouldEqual 10
+    pairing.values.toSet shouldEqual Set("a", "b", "c", "d", "e", "f", "g", "h", "i", "k")
+    pairing.foreach{case (v,k) => v shouldNot be(k)}
+  }
+
+  it should "output self pairing in case of one name" in {
+    val pairing = (new SecretSanta).generateGiftPairing(List("James"))
+    pairing.size shouldEqual 1
+    pairing.get("James") should contain("James")
+  }
 
 }
