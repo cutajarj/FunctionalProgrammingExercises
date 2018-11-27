f51b482eaac8b6cbd4afba3d19bb9b3079cac08b
cutajarj2 <james.cutajar@blip.pt>
2018-11-27 13:00:01 +0000

    adding doc to swap elements

--- src/main/scala/com/cutajarjames/exercise7/SwapElements.scala
+++ src/main/scala/com/cutajarjames/exercise7/SwapElements.scala
@@ -1,5 +1,18 @@
 package com.cutajarjames.exercise7
 
+
+/**
+  * Implement this function accepting a list of any objects and swapping the first and second element
+  * of the list. Here are some examples
+  *
+  * Examples:
+  *
+  * swapFirstAndSecond(List(1,2)) should return List(2,1)
+  * swapFirstAndSecond(List(1,2,3)) should return List(2,1,3)
+  * swapFirstAndSecond(List(1)) should return List(1)
+  * swapFirstAndSecond(List()) should return List()
+  *
+  */
 class SwapElements {
   def swapFirstAndSecond(list: List[Any]): List[Any] = List(123)
 }
