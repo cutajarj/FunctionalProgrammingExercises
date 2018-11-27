e3086afe2145b931aee2aa1a1873df5fb627539f
cutajarj2 <james.cutajar@blip.pt>
2018-11-27 12:28:50 +0000

    adding doc on simple solver

--- src/main/scala/com/cutajarjames/exercise6/SimpleSolver.scala
+++ src/main/scala/com/cutajarjames/exercise6/SimpleSolver.scala
@@ -10,6 +10,17 @@ case class Multiplication(op1: Double, op2: Double) extends Expression(op1, op2)
 
 case class Division(op1: Double, op2: Double) extends Expression(op1, op2)
 
+/**
+  * Implement this function accepting an expression class class and returning the
+  * answer if the expression is defined
+  *
+  *
+  * Examples:
+  *
+  * solve(Addition(1,1) should return Some(1)
+  * solve(Multiplication(3,2) should return Some(6)
+  * solve(Division(3,0) should return None
+  */
 class SimpleSolver {
   def solve(expression: Expression): Option[Double] = Some(0.0)
 }
